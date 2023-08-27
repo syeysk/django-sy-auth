8d304cf9b498c6fe00f5f3b28f820fa22deae003
shyzik93 <shyzik93@mail.ru>
2023-08-27 17:43:56 +0300

    registrate modeladmin for externuser

--- auth_service/admin.py
+++ auth_service/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from auth_service.models import AuthUser
+from auth_service.models import AuthUser, ExternAuthUser
 
 
 admin.site.register(AuthUser, UserAdmin)
+
+
+class ExternAuthUserAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ExternAuthUser, ExternAuthUserAdmin)
