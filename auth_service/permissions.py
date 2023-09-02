d05d54ceb274a73d75e6c52e1798659bc4597479
shyzik93 <shyzik93@mail.ru>
2023-09-02 15:28:57 +0300

    delete `print`

--- auth_service/permissions.py
+++ auth_service/permissions.py
@@ -4,5 +4,4 @@ from rest_framework.permissions import BasePermission
 
 class CheckTokenForAllMicroservices(BasePermission):
     def has_permission(self, request, view):
-        print(request.auth, set(settings.MICROSERVICES_TOKENS.values()))
         return request.auth in set(settings.MICROSERVICES_TOKENS.values())  # TODO: реализовать нормально!
