d82a27e07c9c806893489d24769745349eee9c0d
shyzik93 <shyzik93@mail.ru>
2023-09-21 23:47:34 +0300

    more safe, add task

--- server/settings.py
+++ server/settings.py
@@ -63,7 +63,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'sqlite3.db',
+        'NAME': BASE_DIR / '.sqlite3.db',
     }
 }
 
@@ -74,7 +74,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'debug.log',
+            'filename': BASE_DIR / '.debug.log',
         },
     },
     'loggers': {
